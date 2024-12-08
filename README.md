33f4007e0bcd91b1269f573491e7cc7bf30cf863
imadusanka <isurumadusanka398@gmail.com>
2024-12-08 20:57:15 +0530

    chore: added express

---
+++ README.md
@@ -0,0 +1,1 @@
+# csc554-backend
---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const PORT = 3001;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
